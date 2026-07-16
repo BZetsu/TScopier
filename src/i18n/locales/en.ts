e0321e74a9d81ffafbe5c672b8eb34f63a39f289
Osodi Software Co. <dev@osodi.org>
2026-07-16 14:35:43 +0100

    ok

--- src/i18n/locales/en.ts
+++ src/i18n/locales/en.ts
@@ -125,7 +125,7 @@ export const en: Translations = {
     extraAccountUnit: '$10/account/month',
     extraAccountUnitAnnual: '$96/account/year',
     subscribe: 'Subscribe',
-    startTrial: 'Start 10-day Free Trial',
+    startTrial: 'Get started for free',
     trialDays: '10-day free trial',
     features: 'Features',
     popular: 'Most Popular',
